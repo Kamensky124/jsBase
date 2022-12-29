3942b437ac4431af4fa3cb884a96cd0d5118b716
Kamensky Andrey <104049470+Kamensky124@users.noreply.github.com>
2022-12-29 10:59:16 +0700

    Update main.js

--- main.js
+++ main.js
@@ -2,9 +2,9 @@ const MAX = 100
 const MIN = 0
 const ATTEMPTS_NUMBER = 10
 
-let getPlayerName() => {
+const getPlayerName = () => {
   const playerName = prompt("Enter your Name");
   return playerName;
-};
+}
 
-const playerName = getPlayerName()
+const playerName = getPlayerName();

fac0a4e1ef76a2a0b3437074b8fbb9a09eb69302
Kamensky Andrey <104049470+Kamensky124@users.noreply.github.com>
2022-12-29 10:02:29 +0700

    try gitpen

---
+++ main.js
@@ -0,0 +1,10 @@
+const MAX = 100
+const MIN = 0
+const ATTEMPTS_NUMBER = 10
+
+let getPlayerName() => {
+  const playerName = prompt("Enter your name")
+  return playerName;
+};
+
+const playerName = getPlayerName()
